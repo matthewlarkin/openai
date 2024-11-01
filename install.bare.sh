54fe03a2d422897c879af464453b3325e90fbe78
Matthew Larkin <info@groveos.com>
2024-11-01 08:42:19 -0400

    - adds install script (for https://install.bare.sh)

---
+++ install.bare.sh
@@ -0,0 +1,20 @@
+# Install from https://install.bare.sh
+
+# download raw user content from github
+sudo curl -s https://raw.githubusercontent.com/matthewlarkin/bare.sh/root/bare.sh -o /usr/local/bin/bare.sh && sudo chmod +x /usr/local/bin/bare.sh
+
+# test bare.sh random number (should return a number)
+random_number=$(bare.sh random number)
+
+# if random number is not a number, then remove the file
+if ! [[ $random_number =~ ^[0-9]+$ ]]; then
+  sudo rm /usr/local/bin/bare.sh
+  echo "Failed to install bare.sh"
+  exit 1
+fi
+
+# if random number is a number, then install was successful
+echo "✅ Successfully installed bare.sh"
+echo "✅ You can now run 'bare.sh' from the command line."
+echo "☑️ Visit samples.bare.sh for sample usage."
+echo "☑️ If future updates are available, you can run 'bare.sh --upgrade' to upgrade to the latest version."
